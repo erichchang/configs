2dde5979a86500bb503fe139592692fb89d000fb
Eric Chang <eric@horay.net>
2023-12-29 15:10:31 -0800

    cleaning up init file

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,10 +1,3 @@
--- formatting
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-vim.g.mapleader = " "
-
 -- lazy nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
@@ -19,35 +12,6 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-local plugins = {
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-  {
-    'nvim-telescope/telescope.nvim', tag = '0.1.5',
-    dependencies = { 'nvim-lua/plenary.nvim' }
-  },
-  {
-    {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}
-  }
-}
-local opts = {}
+require("vim-options")
+require("lazy").setup("plugins")
 
-require("lazy").setup(plugins, opts)
-
--- telescope, fzf, rg
-local builtin = require("telescope.builtin")
-vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-
--- treesitter
-local config = require("nvim-treesitter.configs")
-config.setup({
-  ensure_installed = {"lua", "javascript", "python", "c", "vim", "java", "html"},
-  highlight = { enable = true },
-  indent = { enable = true },
-})
-
--- color scheme
-require("catppuccin").setup({
-  flavour = "macchiato"
-})
-vim.cmd.colorscheme "catppuccin"
